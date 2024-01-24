7ed0feebf3c68f89d3116eaf5571cf0cbdb8c57e
gongtao245 <gongtao245@gmail.com>
2024-01-24 22:38:14 +0800

    add Sepolia network

--- hardhat.config.js
+++ hardhat.config.js
@@ -44,5 +44,9 @@ module.exports = {
       url: "https://optimism-mainnet.infura.io/v3/" + process.env.INFURA_ID,
       accounts: mnemonic()
     },
+    Sepolia: {
+      url: "https://sepolia.infura.io/v3/" + process.env.INFURA_ID,
+      accounts: mnemonic()
+    },
   },
 };
