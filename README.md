8e9745d94a336892151709b268a62e2e04602220
liberhe <411497616@qq.com>
2024-01-27 18:27:01 +0800

    add arbi and scroll

--- README.md
+++ README.md
@@ -10,7 +10,15 @@ npx hardhat  compile
 
 npx hardhat run scripts/redpacket/2-deployHappyRedPacket.js --network sepolia
 
+npx hardhat run scripts/redpacket/2-deployHappyRedPacket.js --network scroll
+
+npx hardhat run scripts/redpacket/2-deployHappyRedPacket.js --network arbitrum
 
 npm run format
+
+
+npx hardhat verify --network arbitrum 0x075FE5D2CD5D22D744Da94d81658143abf49D589
+npx hardhat verify --network scroll 0xC90D844679C0eFEB37522c43711D4856d192BD62
+
 
 ```
--- hardhat.config.js
+++ hardhat.config.js
@@ -34,10 +34,6 @@ module.exports = {
       url: "https://mainnet.infura.io/v3/" + process.env.INFURA_ID, //<---- YOUR INFURA ID! (or it won't work)
       accounts: mnemonic(),
     },
-    ropsten: {
-      url: "https://ropsten.infura.io/v3/" + process.env.INFURA_ID, //<---- YOUR INFURA ID! (or it won't work)
-      accounts: mnemonic(),
-    },
     sepolia: {
       url: "https://sepolia.infura.io/v3/" + process.env.INFURA_ID, //<---- YOUR INFURA ID! (or it won't work)
       accounts: mnemonic(),
@@ -50,13 +46,32 @@ module.exports = {
       url: "https://optimism-mainnet.infura.io/v3/" + process.env.INFURA_ID,
       accounts: mnemonic(),
     },
-    Sepolia: {
-      url: "https://sepolia.infura.io/v3/" + process.env.INFURA_ID,
-      accounts: mnemonic(),
+    arbitrum: {
+      url: 'https://arb1.arbitrum.io/rpc',
+      accounts: 
+        mnemonic(),
+    },
+    scroll: {
+      url: "https://rpc.scroll.io",
+      accounts: 
+        mnemonic()
     },
   },
   etherscan: {
+    //apiKey: process.env.SCROLL_KEY,
+    //apiKey: process.env.ARBI_KEY,
     apiKey: process.env.EHTERSCAN_KEY,
+
+    customChains: [
+      {
+        network: 'scroll',
+        chainId: 534352,
+        urls: {
+          apiURL: 'https://api.scrollscan.com/api',
+          browserURL: 'https://scrollscan.com/',
+        },
+      },
+    ],
   },
   sourcify: {
     // Disabled by default
