3a6d49c4797b7f06177b842e2251c2fd45aada02
0x-stan <744428333@qq.com>
2024-01-27 16:31:27 +0800

    update redpacket.test.js

--- test/redpacket.test.js
+++ test/redpacket.test.js
@@ -51,7 +51,7 @@ describe("redpacket", function () {
 
         // Root
         treeRoot = merkleTree.getHexRoot().toString("hex");
-        console.log("MerkleTree root", treeRoot);
+        // console.log("MerkleTree root", treeRoot);
 
         groth16Verifier = await deploy("Groth16Verifier", owner);
         redPacket = await deploy("HappyRedPacket", owner);
@@ -75,25 +75,46 @@ describe("redpacket", function () {
         totalAmount,
         ifrandom,
         hashLock = ZERO_BYTES32,
+        duration = 1 * 24 * 60 * 60,
+        message = "some message",
     ) {
-        const message = "some message";
+        const name = "Redpacket Name";
         const total_tokens = utils.parseUnits(totalAmount, 18);
         const redpacketId = keccak256(
             encodePacked(["address", "string"], [owner.address, message]),
         );
-        const tx = await redPacket.connect(owner).create_red_packet(
-            treeRoot,
-            hashLock,
-            3, // number
-            ifrandom, // ifrandom
-            1 * 24 * 60 * 60, // 1day
-            message, // message
-            "Redpacket Name",
-            1, // token_type
-            erc20.address,
-            total_tokens,
-        );
-        await tx.wait();
+        const blockNumber = await ethers.provider.getBlockNumber();
+        const block = await ethers.provider.getBlock(blockNumber);
+        const creation_time = block.timestamp + 1;
+
+        await expect(
+            redPacket.connect(owner).create_red_packet(
+                treeRoot,
+                hashLock,
+                3, // number
+                ifrandom, // ifrandom
+                duration, // 1day
+                message, // message
+                name,
+                1, // token_type
+                erc20.address,
+                total_tokens,
+            ),
+        )
+            .to.emit(redPacket, "CreationSuccess")
+            .withArgs(
+                total_tokens,
+                redpacketId,
+                name,
+                message,
+                owner.address,
+                creation_time,
+                erc20.address,
+                3,
+                ifrandom,
+                duration,
+                hashLock,
+            );
 
         return {
             redpacketId,
@@ -103,6 +124,119 @@ describe("redpacket", function () {
     }
 
     describe("Normal Redpacket", async () => {
+        it("create_red_packet() check require conditions", async () => {
+            await expect(
+                redPacket.connect(owner).create_red_packet(
+                    treeRoot,
+                    ZERO_BYTES32,
+                    3, // number
+                    true, // ifrandom
+                    1 * 24 * 60 * 60, // 1day
+                    "message", // message
+                    "Redpacket Name",
+                    1, // token_type
+                    erc20.address,
+                    2, // total_tokens
+                ),
+            ).to.revertedWith("tokens > #packets");
+
+            await expect(
+                redPacket.connect(owner).create_red_packet(
+                    treeRoot,
+                    ZERO_BYTES32,
+                    0, // number
+                    true, // ifrandom
+                    1 * 24 * 60 * 60, // 1day
+                    "message", // message
+                    "Redpacket Name",
+                    1, // token_type
+                    erc20.address,
+                    utils.parseUnits("300", 18), // total_tokens
+                ),
+            ).to.revertedWith("At least 1 recipient");
+
+            await expect(
+                redPacket.connect(owner).create_red_packet(
+                    treeRoot,
+                    ZERO_BYTES32,
+                    512, // number
+                    true, // ifrandom
+                    1 * 24 * 60 * 60, // 1day
+                    "message", // message
+                    "Redpacket Name",
+                    1, // token_type
+                    erc20.address,
+                    utils.parseUnits("300", 18), // total_tokens
+                ),
+            ).to.revertedWith("At most 511 recipients");
+
+            await expect(
+                redPacket.connect(owner).create_red_packet(
+                    treeRoot,
+                    ZERO_BYTES32,
+                    3, // number
+                    true, // ifrandom
+                    1 * 24 * 60 * 60, // 1day
+                    "message", // message
+                    "Redpacket Name",
+                    2, // token_type
+                    erc20.address,
+                    utils.parseUnits("300", 18), // total_tokens
+                ),
+            ).to.revertedWith("Unrecognizable token type");
+
+            await expect(
+                redPacket.connect(owner).create_red_packet(
+                    treeRoot,
+                    ZERO_BYTES32,
+                    3, // number
+                    true, // ifrandom
+                    1 * 24 * 60 * 60, // 1day
+                    "message", // message
+                    "Redpacket Name",
+                    1, // token_type
+                    erc20.address,
+                    utils.parseUnits("0.3", 18), // total_tokens
+                ),
+            ).to.revertedWith("At least 0.1 for each user");
+
+            await expect(
+                redPacket.connect(owner).create_red_packet(
+                    treeRoot,
+                    ZERO_BYTES32,
+                    3, // number
+                    true, // ifrandom
+                    1 * 24 * 60 * 60, // 1day
+                    "message", // message
+                    "Redpacket Name",
+                    0, // token_type
+                    erc20.address,
+                    utils.parseUnits("3", 18), // total_tokens
+                ),
+            ).to.revertedWith("No enough ETH");
+
+            // @todo revertedWith("#received > #packets")
+
+            const { redpacketId, total_tokens } = await createRedpacket(
+                "300",
+                true,
+            );
+            await expect(
+                redPacket.connect(owner).create_red_packet(
+                    treeRoot,
+                    ZERO_BYTES32,
+                    3, // number
+                    true, // ifrandom
+                    1 * 24 * 60 * 60, // 1day
+                    "some message", // message
+                    "Redpacket Name",
+                    1, // token_type
+                    erc20.address,
+                    utils.parseUnits("300", 18), // total_tokens
+                ),
+            ).to.revertedWith("Redpacket already exists");
+        });
+
         it("create_red_packet() no password", async () => {
             const { redpacketId, total_tokens } = await createRedpacket(
                 "300",
@@ -116,22 +250,106 @@ describe("redpacket", function () {
         });
 
         it("claimOrdinaryRedpacket(): Shuold all member could claim.", async () => {
-            const { redpacketId, total_tokens } = await createRedpacket("300");
+            const duration = 1 * 24 * 60 * 60;
+            const { redpacketId, total_tokens } = await createRedpacket(
+                "300",
+                false,
+                ZERO_BYTES32,
+                duration,
+            );
+
+            const snapshotId = await ethers.provider.send("evm_snapshot", []);
+
+            let merkleProof;
+
+            merkleProof = merkleTree.getHexProof(hashToken(owner.address));
+
+            await ethers.provider.send("evm_increaseTime", [duration]);
+            await expect(
+                redPacket
+                    .connect(owner)
+                    .claimOrdinaryRedpacket(redpacketId, merkleProof),
+            ).to.revertedWith("Expired");
+            await ethers.provider.send("evm_revert", [snapshotId]);
+
+            merkleProof = merkleTree.getHexProof(hashToken(alice.address));
+            await expect(
+                redPacket
+                    .connect(owner)
+                    .claimOrdinaryRedpacket(redpacketId, merkleProof),
+            ).to.revertedWith("Verification failed, forbidden");
 
             for (let user of [owner, alice, bob]) {
                 const beforeBalance = await erc20.balanceOf(user.address);
-                const merkleProof = merkleTree.getHexProof(
-                    hashToken(user.address),
-                );
-                const tx = await redPacket
+                merkleProof = merkleTree.getHexProof(hashToken(user.address));
+                await redPacket
                     .connect(user)
                     .claimOrdinaryRedpacket(redpacketId, merkleProof);
-                await tx.wait();
                 const afterBalance = await erc20.balanceOf(user.address);
                 expect(afterBalance.sub(beforeBalance)).to.equal(
                     total_tokens.div(3),
                 );
+
+                await expect(
+                    redPacket
+                        .connect(user)
+                        .claimOrdinaryRedpacket(redpacketId, merkleProof),
+                ).to.revertedWith(user !== bob ? "Already claimed" : "Out of stock");
             }
+
+        });
+
+        it("refund(): Should refund after expired", async () => {
+            const duration = 1 * 60 * 60;
+            const { redpacketId, total_tokens } = await createRedpacket(
+                "300",
+                false,
+                ZERO_BYTES32,
+                duration,
+            );
+
+            const snapshotId = await ethers.provider.send("evm_snapshot", []);
+            await expect(
+                redPacket.connect(alice).refund(redpacketId),
+            ).to.revertedWith("Creator Only");
+
+            await expect(
+                redPacket.connect(owner).refund(redpacketId),
+            ).to.revertedWith("Not expired yet");
+
+            // claim all tokens
+            for (let user of [owner, alice, bob]) {
+                const merkleProof = merkleTree.getHexProof(
+                    hashToken(user.address),
+                );
+                await redPacket
+                    .connect(user)
+                    .claimOrdinaryRedpacket(redpacketId, merkleProof);
+            }
+
+            await ethers.provider.send("evm_increaseTime", [duration]);
+
+            await expect(
+                redPacket.connect(owner).refund(redpacketId),
+            ).to.revertedWith("None left in the red packet");
+
+            // revert to snapshot
+            await ethers.provider.send("evm_revert", [snapshotId]);
+            await ethers.provider.send("evm_increaseTime", [duration]);
+
+            const beforeBalance = await erc20.balanceOf(owner.address);
+
+            await expect(redPacket.connect(owner).refund(redpacketId))
+                .to.emit(redPacket, "RefundSuccess")
+                .withArgs(
+                    redpacketId,
+                    erc20.address,
+                    total_tokens,
+                    ZERO_BYTES32,
+                );
+
+            const afterBalance = await erc20.balanceOf(owner.address);
+            expect(afterBalance.sub(beforeBalance)).to.equal(total_tokens);
         });
     });
 
@@ -159,15 +377,49 @@ describe("redpacket", function () {
                 false,
                 hashLock,
             );
+            const { redpacketId: redpacketId2 } = await createRedpacket(
+                "300",
+                false,
+                ZERO_BYTES32,
+                1 * 24 * 60 * 60,
+                "some other message",
+            );
+
+            let merkleProof;
 
             const wrong_password = "This is a wrong password";
-
             const { proof: wrong_zkproof, publicSignals: wrong_publicSignals } =
                 await calculateZKProof(wrong_password);
 
             expect(toHex(BigInt(wrong_publicSignals[0]))).to.not.equal(
                 hashLock,
             );
+
+            merkleProof = merkleTree.getHexProof(hashToken(owner.address));
+            await expect(
+                redPacket
+                    .connect(owner)
+                    .claimPasswordRedpacket(
+                        redpacketId2,
+                        merkleProof,
+                        wrong_zkproof.a,
+                        wrong_zkproof.b,
+                        wrong_zkproof.c,
+                    ),
+            ).to.revertedWith("Not password redpacket");
+
+            merkleProof = merkleTree.getHexProof(hashToken(owner.address));
+            await expect(
+                redPacket
+                    .connect(owner)
+                    .claimPasswordRedpacket(
+                        redpacketId,
+                        merkleProof,
+                        wrong_zkproof.a,
+                        wrong_zkproof.b,
+                        wrong_zkproof.c,
+                    ),
+            ).to.revertedWith("ZK Verification failed, wrong password");
 
             const { proof: correct_zkproof, publicSignals } =
                 await calculateZKProof(correct_password);
@@ -178,9 +430,7 @@ describe("redpacket", function () {
 
             for (let user of [owner, alice, bob]) {
                 const beforeBalance = await erc20.balanceOf(user.address);
-                const merkleProof = merkleTree.getHexProof(
-                    hashToken(user.address),
-                );
+                merkleProof = merkleTree.getHexProof(hashToken(user.address));
                 await expect(
                     redPacket
                         .connect(user)
@@ -204,6 +454,29 @@ describe("redpacket", function () {
                 const afterBalance = await erc20.balanceOf(user.address);
                 expect(afterBalance.sub(beforeBalance)).to.equal(claimed_value);
             }
+        });
+
+        it("refund(): Should refund after expired", async () => {
+            const duration = 1 * 60 * 60;
+            const password = "This is a password";
+            const hashLock = await calculatePublicSignals(password);
+            const { redpacketId, total_tokens } = await createRedpacket(
+                "300",
+                false,
+                hashLock,
+                duration,
+            );
+
+            await ethers.provider.send("evm_increaseTime", [duration]);
+
+            const beforeBalance = await erc20.balanceOf(owner.address);
+
+            await expect(redPacket.connect(owner).refund(redpacketId))
+                .to.emit(redPacket, "RefundSuccess")
+                .withArgs(redpacketId, erc20.address, total_tokens, hashLock);
+
+            const afterBalance = await erc20.balanceOf(owner.address);
+            expect(afterBalance.sub(beforeBalance)).to.equal(total_tokens);
         });
     });
 });
