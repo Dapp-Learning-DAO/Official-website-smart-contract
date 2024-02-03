3d08be1268c24beb6d5a2715e0bb3d68245803a6
0x-stan <744428333@qq.com>
2024-02-03 15:01:28 +0800

    upgrade dependencies: ehters v5 -> v6

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-ethers");
+// require("@nomiclabs/hardhat-waffle");
+// require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-toolbox");
 require('@nomicfoundation/hardhat-verify');
 
 require("dotenv").config();
